15231c37ee3c92c84d5dbdd06563904111b654fc
Hariharasudhan27 <93508945+Hariharasudhan27@users.noreply.github.com>
2023-10-07 15:47:00 +0530

    Add files via upload

---
+++ hera/app.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items: 1, // Number of items to display at a time
+        loop: true, // Infinite loop
+        autoplay: true, // Autoplay the slider
+        autoplayTimeout: 2000, // Autoplay interval in milliseconds
+        autoplayHoverPause: true // Pause autoplay on hover
+    });
+});
